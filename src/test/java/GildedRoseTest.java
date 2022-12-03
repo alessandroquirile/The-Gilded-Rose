dc2cc237f4dbb5964d8e4c0ec350c89b782bcd59
alessandroquirile <a.quirile@studenti.unina.it>
2022-12-03 19:38:49 +0100

    added some test cases

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -5,13 +5,48 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
+    private Item[] items;
+    private GildedRose app;
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void sellInShouldBeUpdatedEveryDay() {
+        items = new Item[] {
+                new Item("", 30, 10)
+        };
+        app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(29, items[0].sellIn);
     }
 
+    @Test
+    void regularItemsShouldDegradeQualityByOneEveryDay() {
+        items = new Item[] {
+                new Item("", 30, 10)
+        };
+        app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, items[0].quality);
+    }
+
+    @Test
+    void regularItemsShouldDegradeTwiceFasterWhenSellInHasPassed() {
+        items = new Item[] {
+                new Item("", -1, 10),
+                new Item("", 0, 10)
+        };
+        app = new GildedRose(items);
+        app.updateQuality();
+        for (Item item : items)
+            assertEquals(8, item.quality);
+    }
+
+    @Test
+    void regularItemsQualityShouldNotBeNegative() {
+        items = new Item[] {
+                new Item("", 30, 0)
+        };
+        app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, items[0].quality);
+    }
 }
